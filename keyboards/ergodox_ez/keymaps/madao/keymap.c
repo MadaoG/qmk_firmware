f4d0324b5977037da1369459133814be2ac8581c
Daniel Ceglinski <danielceglinski@web.de>
2017-11-26 18:36:10 +0100

    externalized gauss functions and template

--- keyboards/ergodox_ez/keymaps/madao/keymap.c
+++ keyboards/ergodox_ez/keymaps/madao/keymap.c
@@ -4,6 +4,7 @@
 #include "action_layer.h"
 #include "keymap_extras/keymap_german.h"
 #include "mousekey.h"
+#include "gauss.h"
 
 // #define   _BS   0    // base layer
 #define   _EN   0    // english layer
@@ -25,7 +26,6 @@
 #define _______ KC_TRNS
 
 uint8_t last_layer = _BS; // check if layer was changed
-uint16_t t = 0;         // time count for leds: 0 -- 1000
 
 enum {
  CT_CLN,
@@ -716,90 +716,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  }}}
 
 // led functions {{{
-int gauss_curve(int x, int a, int m, int s)
-{
-  return (int)floor(a * exp(-0.5 * (x - m) * (x - m) / (s * s)));
-}
-
-// {{{ brightness functions
-int brightness_very_slow(int offset)
-{
-  return gauss_curve(t, 255, offset, 120);
-}
-
-int brightness_slow(int offset)
-{
-  return gauss_curve(t, 255, offset, 100);
-}
-
-int brightness_middle(int offset)
-{
-  return gauss_curve(t, 255, offset, 70);
-}
-
-int brightness_fast(int offset)
-{
-  return gauss_curve(t, 255, offset, 20);
-}
-
-int brightness_very_fast(int offset)
-{
-  return gauss_curve(t, 255, offset, 10);
-}
-
-int brightness_fast_double(int offset1, int offset2)
-{
-  return brightness_fast(offset1) 
-       + brightness_fast(offset2);
-}
-
-int brightness_very_fast_double(int offset1, int offset2)
-{
-  return brightness_very_fast(offset1) 
-       + brightness_very_fast(offset2);
-}
-
-int brightness_very_fast_triple(int offset1, int offset2, int offset3)
-{
-  return brightness_very_fast(offset1) 
-       + brightness_very_fast(offset2)
-       + brightness_very_fast(offset3);
-}
-// }}}
-
-// {{{ sset functions
-void ergodox_set_red(int brightness)
-{
-  if (brightness == 0) {
-    ergodox_right_led_1_off();
-  } else {
-    brightness > 255 ? brightness = 255 :
-    ergodox_right_led_1_on();
-    ergodox_right_led_1_set(brightness);
-  }
-}
-
-void ergodox_set_green(int brightness)
-{
-  if (brightness == 0) {
-    ergodox_right_led_2_off();
-  } else {
-    brightness > 255 ? brightness = 255 :
-    ergodox_right_led_2_on();
-    ergodox_right_led_2_set(brightness);
-  }
-}
-
-void ergodox_set_blue(int brightness)
-{
-  if (brightness == 0) {
-    ergodox_right_led_3_off();
-  } else {
-    brightness > 255 ? brightness = 255 :
-    ergodox_right_led_3_on();
-    ergodox_right_led_3_set(brightness);
-  }
-}
 // }}}
 
 // }}}
@@ -957,55 +873,3 @@ void matrix_scan_user(void) {
       ergodox_right_led_3_off();
     }
 };
-
-
-/*
-    .---------.-------.-------.-------.-------.-------.-------.           .-------.-------.-------.-------.-------.-------.---------.
-    |    _    |   _   |   _   |   _   |   _   |   _   |   _   |           |   _   |   _   |   _   |   _   |   _   |   _   |    _    |
-    |         |       |       |       |       |       |       |           |       |       |       |       |       |       |         |
-    |---------|-------|-------|-------|-------|-------|-------|           |-------|-------|-------|-------|-------|-------|---------|
-    |    _    |   _   |   _   |   _   |   _   |   _   |       |           |       |   _   |   _   |   _   |   _   |   _   |    _    |
-    |         |       |       |       |       |       |   _   |           |   _   |       |       |       |       |       |         |
-    |---------|-------|-------|-------|-------|-------|       |           |       |-------|-------|-------|-------|-------|---------|
-    |    _    |   _   |   _   |   _   |   _   |   _   |_______|           |_______|   _   |   _   |   _   |   _   |   _   |    _    |
-    |         |       |       |       |       |       |       |           |       |       |       |       |       |       |         |
-    |---------|-------|-------|-------|-------|-------|   _   |           |   _   |-------|-------|-------|-------|-------|---------|
-    |    _    |   _   |   _   |   _   |   _   |   _   |       |           |       |   _   |   _   |   _   |   _   |   _   |    _    |
-    |         |       |       |       |       |       |       |           |       |       |       |       |       |       |         |
-    '---------|-------|-------|-------|-------|---------------'           '---------------|-------|-------|-------|-------|---------'
-      |   _   |   _   |   _   |   _   |   _   |                                           |   _   |   _   |   _   |   _   |   _   |
-      |       |       |       |       |       |                                           |       |       |       |       |       |
-      '-------'-------'-------'-------'-------'                                           '-------'-------'-------'-------'-------'
-                                                 .-------.-------.     .-------.-------.
-                                                 |   _   |   _   |     |   _   |   _   |
-                                                 |       |       |     |       |       |
-                                         .-------|-------|-------|     |-------|-------|-------.
-                                         |       |       |   _   |     |   _   |       |       |
-                                         |       |       |       |     |       |       |       |
-                                         |   _   |   _   |-------|     |-------|   _   |   _   |
-                                         |       |       |   _   |     |   _   |       |       |
-                                         |       |       |       |     |       |       |       |
-                                         '-------'-------'-------'     '-------'-------'-------'
- */
-
- // [_] = KEYMAP(
- // _______,      _______,      _______,      _______,      _______,      _______,      _______,
- // _______,      _______,      _______,      _______,      _______,      _______,      _______,
- // _______,      _______,      _______,      _______,      _______,      _______,      /*---*/
- // _______,      _______,      _______,      _______,      _______,      _______,      _______,
- // _______,      _______,      _______,      _______,      _______,
- // _______,      _______,                         //
- // _______,                                       // thumb keys
- // _______,      _______,      _______,           //
-
- // _______,      _______,      _______,      _______,      _______,      _______,      _______,
- // _______,      _______,      _______,      _______,      _______,      _______,      _______,
- // /*---*/       _______,      _______,      _______,      _______,      _______,      _______,
- // _______,      _______,      _______,      _______,      _______,      _______,      _______,
- // /*---*/       /*---*/       _______,      _______,      _______,      _______,      _______,
- // _______,      _______,                         //
- // _______,                                       // thumb keys
- // _______,      _______,      _______            //
- // ),
- 
- 
