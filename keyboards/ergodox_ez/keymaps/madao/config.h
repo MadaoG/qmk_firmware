9b602939c8e40e429cd742cfa84137bbdad5a217
Daniel Ceglinski <danielceglinski@web.de>
2017-12-13 19:50:42 +0100

    clean up config file

--- keyboards/ergodox_ez/keymaps/madao/config.h
+++ keyboards/ergodox_ez/keymaps/madao/config.h
@@ -1,6 +1,7 @@
 
 #ifndef CONFIG_USER_H
-#define CONFIG_USER_H
+  #define CONFIG_USER_H
+#endif
 
 #include "../../config.h"
 
@@ -8,4 +9,9 @@
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 2
 
+#ifndef NO_DEBUG
+  #define NO_DEBUG
+#endif
+#ifndef NO_PRINT
+  #define NO_PRINT
 #endif
