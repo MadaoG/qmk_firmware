d612aba8fb35779a2010510dd459bed5019951ba
Daniel Ceglinski <danielceglinski@web.de>
2017-12-29 19:02:13 +0100

    use short keycode for enter key

--- keyboards/ergodox_ez/keymaps/madao/keymap.c
+++ keyboards/ergodox_ez/keymaps/madao/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //
  _______,            _______,            /*---*/           //
  _______,            /*---*/             /*---*/           // thumb keys right
- KC_DEL,             KC_ENTER,           KC_SPACE          //
+ KC_DEL,             KC_ENT,             KC_SPACE          //
 ),
 
 [_GE] = KEYMAP(
