f936b69ef7cbbece16c709d8004b56674507eb77
Daniel Ceglinski <danielceglinski@web.de>
2018-04-29 11:38:00 +0200

    correct base layer doc

--- keyboards/ergodox_ez/keymaps/madao/keymap.c
+++ keyboards/ergodox_ez/keymaps/madao/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                       .-------|-------|-------|     |-------|-------|-------.
  *                                       |       |       |       |     |       |       |       |
  *                                       |       |       |       |     |       |       |       |
- *                                       |  TAB  | SHIFT |-------|     |-------| ENTER | SPACE |
+ *                                       | SHIFT |  TAB  |-------|     |-------| ENTER | SPACE |
  *                                       |       |       | BACK  |     | DEL   |       |       |
  *                                       |       |       | SPACE |     |       |       |       |
  *                                       '-------'-------'-------'     '-------'-------'-------'
