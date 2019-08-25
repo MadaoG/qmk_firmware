8c2be429c83ee8327b2605de5b84db1e152fc8da
Daniel Ceglinski <danielceglinski@web.de>
2019-08-25 10:48:21 +0200

    autoindent `macro_functions.h`

--- keyboards/ergodox_ez/keymaps/madao/macro_functions.h
+++ keyboards/ergodox_ez/keymaps/madao/macro_functions.h
@@ -5,48 +5,48 @@
 #define _CHLY M(mc_change_layout)
 
 enum { 
-  mc_sh_clicks,
-  mc_hat,
-  mc_tick,
-  mc_backtick,
-  mc_change_layout,
+    mc_sh_clicks,
+    mc_hat,
+    mc_tick,
+    mc_backtick,
+    mc_change_layout,
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 
     if (record->event.pressed) {
-      switch(id) {
-        case mc_sh_clicks:
-          for (int i = 0; i < 25; i++) {
-            // mousekey_on(KC_BTN1);
-            // mousekey_send();
-            // wait_ms(10);
-            // mousekey_off(KC_BTN1);
-            // mousekey_send();
-            // wait_ms(10);
-          }
-          break;
-        case mc_tick:
-            #if __LANGUAGE__ == __GERMAN__
-          TAP_TWICE(KC_EQL);
-            #endif
-          break;
-        case mc_backtick:
-            #if __LANGUAGE__ == __GERMAN__
-          register_code(KC_LSFT);
-          TAP_TWICE(DE_ACUT);
-          unregister_code(KC_LSFT);
-            #endif
-          break;
-        case mc_hat:
-          TAP_TWICE(KC_GRAVE);
-          break;
-        case mc_change_layout:
-          register_code(KC_LGUI);
-          register_code(KC_SPACE);
-          unregister_code(KC_SPACE);
-          unregister_code(KC_LGUI);
-      }
+        switch(id) {
+            case mc_sh_clicks:
+                for (int i = 0; i < 25; i++) {
+                    // mousekey_on(KC_BTN1);
+                    // mousekey_send();
+                    // wait_ms(10);
+                    // mousekey_off(KC_BTN1);
+                    // mousekey_send();
+                    // wait_ms(10);
+                }
+                break;
+            case mc_tick:
+#if __LANGUAGE__ == __GERMAN__
+                TAP_TWICE(KC_EQL);
+#endif
+                break;
+            case mc_backtick:
+#if __LANGUAGE__ == __GERMAN__
+                register_code(KC_LSFT);
+                TAP_TWICE(DE_ACUT);
+                unregister_code(KC_LSFT);
+#endif
+                break;
+            case mc_hat:
+                TAP_TWICE(KC_GRAVE);
+                break;
+            case mc_change_layout:
+                register_code(KC_LGUI);
+                register_code(KC_SPACE);
+                unregister_code(KC_SPACE);
+                unregister_code(KC_LGUI);
+        }
     }
     return MACRO_NONE;
 };
